6a5ce1ec5d858bd5f18522b9309c3d12e4dcfac2
Anik Saha <anik_saha_001@yahoo.com>
2018-07-17 22:14:04 -0400

    execute the findAll query from config file using sfl4j logger

--- jpa-basic/src/main/java/com/practice/jpabasic/JpaBasicApplication.java
+++ jpa-basic/src/main/java/com/practice/jpabasic/JpaBasicApplication.java
@@ -1,12 +1,32 @@
 package com.practice.jpabasic;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class JpaBasicApplication {
+import com.practice.jpabasic.dao.PersonJdbcDAO;
 
+@SpringBootApplication
+public class JpaBasicApplication implements CommandLineRunner {
+ 
+	@Autowired
+    PersonJdbcDAO personJdbcDao;
+	
+	//sfl4j
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	public static void main(String[] args) {
 		SpringApplication.run(JpaBasicApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		logger.info("All users -> {}" ,  personJdbcDao.findAll());
+	//	personJdbcDao.findAll();
 	}
 }

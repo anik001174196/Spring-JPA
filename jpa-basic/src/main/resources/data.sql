a8483103cd962978058145ff39023ce508a1cc55
Anik Saha <anik_saha_001@yahoo.com>
2018-07-19 21:33:37 -0400

    add lggoing into console

--- jpa-basic/src/main/resources/data.sql
+++ jpa-basic/src/main/resources/data.sql
@@ -8,7 +8,7 @@ create table person
 
 );
 
-insert into person (id, name, location, birth_date) values (10001, 'Anik', 'Chittagong', sysdate());
+insert into person (id, name, location, birth_date) values (10001, 'James', 'Chittagong', sysdate());
 insert into person (id, name, location, birth_date) values (10002, 'Disha', 'Mymensingh', sysdate());
 insert into person (id, name, location, birth_date) values (10003, 'JOhn', 'Canada', sysdate());
 select * from person;

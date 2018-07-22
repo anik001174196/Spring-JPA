74d79b40e394861b11d8700414a49c166b4dbff5
Anik Saha <anik_saha_001@yahoo.com>
2018-07-22 15:38:04 -0400

    basic set up for spring jpa(advanced)

---
+++ advanced/src/main/java/com/learning/jpa/advanced/AdvancedApplication.java
@@ -0,0 +1,12 @@
+package com.learning.jpa.advanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedApplication.class, args);
+	}
+}

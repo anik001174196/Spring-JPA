000a9416fc91198f70df9f8b4a40291d57346723
Anik Saha <anik_saha_001@yahoo.com>
2018-07-29 20:30:38 -0400

    session == persistentCOntext

--- advanced/src/test/java/com/learning/jpa/advanced/StudetRepositoryTest.java
+++ advanced/src/test/java/com/learning/jpa/advanced/StudetRepositoryTest.java
@@ -11,35 +11,56 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.learning.jpa.advanced.entities.Passport;
 import com.learning.jpa.advanced.entities.Student;
 import com.learning.jpa.advanced.repositories.StudentRepository;
 
-
 // context gets launched , tests run and contexts destryed
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes=AdvancedApplication.class)
+@SpringBootTest(classes = AdvancedApplication.class)
 public class StudetRepositoryTest {
 
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
-	
+
 	@Autowired
 	private StudentRepository studentRepository;
-	
+
 	@Autowired
 	private EntityManager entityManager;
-	
-	
+
 	@Test
 	@Transactional // as the class is lazy loaded so if we do not use this
 	// then student.getPassport() will throw an error because data will be
 	// automatically grabbed without this
 	public void findStudentInfoWithPassport_basicTest() {
-		Student student = entityManager.find(Student.class, 20001L);
+		Student student = studentRepository.findById(20001L);
 		logger.info("student -> {}", student);
 		logger.info("passport -> {}", student.getPassport());
 	}
-	
-	
-	
+
+	@Test
+	@Transactional
+	// if student.setName("Anik - updated"); failed then the entire function
+	// operation will be rolled back
+	// create persitence context
+	// Entity manager play with persitent context
+	// session == persistentContext
+	public void someTest() {
+		Student student = studentRepository.findById(20001L);
+		// persistentContext student
+
+		Passport passport = student.getPassport();
+		// persistentContext student & passport
+		passport.setNumber("EA1234");
+
+		// persistentContext student & passport++
+
+		student.setName("Anik - updated");
+		entityManager.flush();
+		// persistentContext student++ & passport++
+
+		logger.info("student -> {}", student);
+		logger.info("passport -> {}", student.getPassport());
+	}
 
 }

96b6d828239cbbffc4240382678391e2423e3569
Haimanti Saha <anik.saha.4096@gmail.com>
2018-07-31 11:52:42 -0400

    add more data

--- advanced/src/main/resources/data.sql
+++ advanced/src/main/resources/data.sql
@@ -16,3 +16,12 @@ insert into student(id, name, passport_id) values(20003, 'Disha', 40002);
 insert into review(id, ratings, description, course_id) values(80001, '5', 'Very Good Course', 10001);
 insert into review(id, ratings, description, course_id) values(80002, '4.5', 'Wonderful Course', 10001);
 insert into review(id, ratings, description, course_id) values(80003, '4', 'Moderate Course', 10003);
+
+insert into student_course(student_id,course_id)
+values(20001,10001);
+insert into student_course(student_id,course_id)
+values(20002,10001);
+insert into student_course(student_id,course_id)
+values(20003,10001);
+insert into student_course(student_id,course_id)
+values(20001,10003);

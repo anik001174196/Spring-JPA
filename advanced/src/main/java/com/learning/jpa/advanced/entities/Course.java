ef5363736e190e298f09e47d5e918da47ee4e57d
Haimanti Saha <anik.saha.4096@gmail.com>
2018-07-30 22:23:38 -0400

    eager vs lazy

--- advanced/src/main/java/com/learning/jpa/advanced/entities/Course.java
+++ advanced/src/main/java/com/learning/jpa/advanced/entities/Course.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
@@ -35,7 +36,10 @@ public class Course {
 	@CreationTimestamp
 	private LocalDateTime createdDate;
 	
-   @OneToMany( mappedBy="course")
+	
+	// eager works with left outer join
+	// lazy just works with just one table
+   @OneToMany( mappedBy="course", fetch=FetchType.EAGER)
 	private List<Review> reviews = new ArrayList<Review>();
 	
 	public Course() {
